8004e7ce2b49c95b6d0f1efa0f41955270fa7cf4
Naz Demirsoy <75198796+nazdemirsoyy@users.noreply.github.com>
2022-06-06 14:09:11 +0200

    Update main.cpp

--- sample_3/main.cpp
+++ sample_3/main.cpp
@@ -3,14 +3,10 @@
 #include <vector>
 #include <string>
 #include <sstream>
-
-/*
-hackaton:ID,name,taskPoint
-sorted by name
-get all the competitor who got the max score before the first failing
-competitior(< 40%)
-*/
-
+/*A company has made a couple of hackathons, many people contributed to those hackathons. The results of those hackathons are stored in a file. Each line in the files of the results starts with the ID of the hackathon (string without white spaces), then the name of the competitor (string without white spaces), then the results of every task the competitor solved on the scale of 1..100 (integer), the competitor solved at least one task!
+GRADE 3
+	get the competitor who got the max score before the first failing competitor (the score is the average
+	of all of the results of one competitor, and a competitor fails if he scored less than 40%) */
 struct Hackaton{
     std::string HackID;
     std::string name;
